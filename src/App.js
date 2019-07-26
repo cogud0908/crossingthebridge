168175bbad85c92023ccf946980d70878c12b323
cogud0908 <cogud0908@naver.com>
2019-07-26 18:16:36 +0900

    경로 오류 디버깅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./Styles/globalStyle";
-import Root from "./Router/Root";
+import Root from "./router/Root";
 
 const App = () => {
   return (
